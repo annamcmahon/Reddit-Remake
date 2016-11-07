85283a3d616205f718958302e036f6e15d311c1a
kquinn1 <kquinn1@nd.edu>
2016-11-06 21:19:54 -0500

    added make post page

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 var browseModule = angular.module('browse', []);
 var loginModule = angular.module('login',[]);
 var discoverModule = angular.module('discover',[]);
+var makePostModule = angular.module('make-post',[]);
 
 // dependencies of app module in []
-var app = angular.module('app', ['ui.router','ui.bootstrap', 'browse', 'login', 'discover']);
+var app = angular.module('app', ['ui.router','ui.bootstrap', 'browse', 'login', 'discover','make-post']);
--- src/partials/browse/browse-controller.js
+++ src/partials/browse/browse-controller.js
@@ -1,4 +1,4 @@
 browseModule.controller('browseController', ['$scope', '$state', '$http', function ($scope, $state, $http) {
 	var vm = this;
-	vm.data = [{postTitle: "Title1", source:"source1", votes: 5},{postTitle: "Title2", source:"source2", votes: 4},{postTitle: "Title3", source:"source3", votes: 3},{postTitle: "Title4", source:"source4", votes:1}];
+	vm.data = [{postTitle: "Title1", source:"source1", votes: 9},{postTitle: "Title2", source:"source2", votes: 7},{postTitle: "Title3", source:"source3", votes: 6},{postTitle: "Title4", source:"source4", votes:3}];
 }]);
