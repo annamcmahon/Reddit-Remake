48d598f62f623a5d973601af2c5e027d567d2fe1
amcmaho4 <amcmaho4@nd.edu>
2016-11-07 03:19:27 -0500

    subreddit page

--- src/partials/browse-subreddit/browse-subreddit-controller.js
+++ src/partials/browse-subreddit/browse-subreddit-controller.js
@@ -2,4 +2,5 @@ browseModule.controller('browseSubredditController', ['$scope', '$state', '$http
 	var vm = this;
 	vm.title = SharedService.getProperty();
 	vm.data = [{postTitle: "Title1", source:"source1", votes: 9},{postTitle: "Title2", source:"source2", votes: 7},{postTitle: "Title3", source:"source3", votes: 6},{postTitle: "Title4", source:"source4", votes:3}];
+  vm.subscribed = false;
 }]);
